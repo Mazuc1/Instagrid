e2972176c02da3dd38ea28898d423a86426edd06
Loïc Mazuc <mazuc.loic@icloud.com>
2021-11-18 16:22:34 +0100

    feat: add layout view with configuration

---
+++ Instagrid/Models/Configuration.swift
@@ -0,0 +1,30 @@
+//
+//  Configuration.swift
+//  Instagrid
+//
+//  Created by Loic Mazuc on 18/11/2021.
+//
+
+import Foundation
+
+struct Configuration {
+    var numberOfPhoto: Int
+    var positions: [Positions]
+    var ratio: [Ratio]
+}
+
+enum Positions {
+    case top
+    case bottom
+    case left
+    case right
+    case topLeft
+    case topRight
+    case bottomLeft
+    case bottomRight
+}
+
+enum Ratio {
+    case quarter
+    case half
+}
--- Instagrid/Views/ViewController.swift
+++ Instagrid/Views/ViewController.swift
@@ -11,8 +11,11 @@ class ViewController: UIViewController {
     
     //  MARK: - Properties
     
+    let marginsViewLayout: CGFloat = 15
+    
     //  MARK: - Outlets
     
+    @IBOutlet weak var viewLayout: UIView!
     @IBOutlet weak var labelInstagrid: UILabel!
     @IBOutlet weak var labelSwipeToShare: UILabel!
     @IBOutlet weak var stackViewLayout: UIStackView!
@@ -24,11 +27,17 @@ class ViewController: UIViewController {
         configureUI()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        createLayout(configuration: Layout.two)
+    }
+    
     //  MARK: - Actions
     
     @IBAction func didSelectLayout(_ sender: UIButton) {
         clearStackView()
         sender.setBackgroundImage(UIImage(named: "Selected_\(sender.tag)"), for: .normal)
+        createLayout(configuration: getConfiguration(for: sender.tag))
     }
     
     //  MARK: - Methods
@@ -41,6 +50,8 @@ class ViewController: UIViewController {
         
         labelSwipeToShare.text = "^\nSwipe up to share"
         labelSwipeToShare.font = UIFont.delm(size: 26)
+        
+        viewLayout.backgroundColor = .darkBlue
     }
     
     private func clearStackView() {
@@ -50,6 +61,57 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    private func getConfiguration(for index: Int) -> Configuration {
+        switch index {
+        case 1: return Layout.one
+        case 2: return Layout.two
+        case 3: return Layout.three
+        default: return Layout.one
+        }
+    }
 
+}
+
+//  MARK: - Create Layout
+
+extension ViewController {
+    
+    private func createLayout(configuration: Configuration) {
+        viewLayout.removeSubviews()
+        for i in 0..<configuration.numberOfPhoto {
+            let size = getSizeFor(ratio: configuration.ratio[i])
+            let point = getPointFor(position: configuration.positions[i])
+            let view = UIView(frame: CGRect(origin: point, size: size))
+            view.layer.cornerRadius = 5
+            view.backgroundColor = .white
+            viewLayout.addSubview(view)
+        }
+    }
+    
+    private func getSizeFor(ratio: Ratio) -> CGSize {
+        switch ratio {
+        case .quarter:
+            let calcul = (viewLayout.frame.width / 2) - 15 - marginsViewLayout / 2
+            return CGSize(width: calcul, height: calcul)
+        case .half:
+            let height = (viewLayout.frame.height / 2) - 15 - marginsViewLayout / 2
+            let width = viewLayout.frame.width - 30
+            return CGSize(width: width, height: height)
+        }
+    }
+    
+    private func getPointFor(position: Positions) -> CGPoint {
+        let centerXPoint = (viewLayout.frame.width / 2) + (marginsViewLayout / 2)
+        let centerYPoint = (viewLayout.frame.height / 2) + (marginsViewLayout / 2)
+        
+        switch position {
+        case .top, .left, .topLeft: return CGPoint(x: 15, y: 15)
+        case .right, .topRight: return CGPoint(x: centerXPoint, y: 15)
+        case .bottom, .bottomLeft: return CGPoint(x: 15, y: centerYPoint)
+        case .bottomRight: return CGPoint(x: centerXPoint, y: centerYPoint)
+        }
+    }
+    
 }
 
