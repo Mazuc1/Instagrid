7f63352e5a9d887285f375749e5c9bb5f069a8b2
Loïc Mazuc <mazuc.loic@icloud.com>
2021-11-19 14:19:15 +0100

    feat: transform view to image

--- Instagrid/Extension/UIView+extension.swift
+++ Instagrid/Extension/UIView+extension.swift
@@ -17,4 +17,11 @@ extension UIView {
             subview.removeFromSuperview()
         }
     }
+    
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
 }
